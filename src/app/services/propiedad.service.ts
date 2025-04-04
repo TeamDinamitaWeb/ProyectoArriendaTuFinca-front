384ad0a505ee857616b6e3380cb0661b80c27ffa
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-04-04 16:06:14 -0500

    Componentes creados

--- src/app/services/propiedad.service.ts
+++ src/app/services/propiedad.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Propiedad } from '../models/Propiedad';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PropiedadService {
 
+private proyectoUrlUsuarios = 'http://localhost/propiedades';
+
   constructor() { }
+
+  getPRopiedades(): Promise<Propiedad[]> {
+    return axios.get<Propiedad[]>(this.proyectoUrlUsuarios)
+      .then(response => response.data)
+      .catch((error) => {
+        console.error('Error obteniendo usuarios:', error);
+        return [];
+      });
+  }
 }
