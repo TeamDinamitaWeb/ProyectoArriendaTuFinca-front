d1ab93cf4a41d8f075e702733fa289cd5dc83f3d
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-04-09 22:05:26 -0500

    Estructura landing page

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [CommonModule],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css'
+})
+export class LandingComponent {
+
+}
