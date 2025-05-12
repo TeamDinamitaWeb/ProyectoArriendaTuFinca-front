c9e5ca2cd9f9bd11be038dc9d5066be51f35580f
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-05-12 01:22:01 -0500

    Service propiedad

--- src/app/services/propiedad.service.ts
+++ src/app/services/propiedad.service.ts
@@ -7,16 +7,62 @@ import axios from 'axios';
 })
 export class PropiedadService {
 
-private proyectoUrlUsuarios = 'http://localhost/propiedades';
+private proyectoUrlPropiedades = 'http://localhost/propiedades';
+private proyectoUrlAllPropiedades = 'http://localhost/propiedades/all-including-deleted';
 
   constructor() { }
 
-  getPRopiedades(): Promise<Propiedad[]> {
-    return axios.get<Propiedad[]>(this.proyectoUrlUsuarios)
+  getPropiedades(): Promise<Propiedad[]> {
+    return axios.get<Propiedad[]>(this.proyectoUrlPropiedades)
       .then(response => response.data)
       .catch((error) => {
-        console.error('Error obteniendo usuarios:', error);
+        console.error('Error obteniendo propiedades:', error);
         return [];
+      });
+  }
+
+  getAllPropiedades(): Promise<Propiedad[]> {
+    return axios.get<Propiedad[]>(this.proyectoUrlAllPropiedades)
+      .then(response => response.data)
+      .catch((error) => {
+        console.error('Error obteniendo todas las propiedades incluyendo las eliminadas:', error);
+        return [];
+      });
+  }
+
+  postPropiedad(propiedad: Propiedad): Promise<Propiedad> {
+    return axios.post<Propiedad>(this.proyectoUrlPropiedades, propiedad)
+      .then(response => response.data)
+      .catch((error) => {
+        console.error('Error creando propiedad:', error);
+        throw error;
+      });
+  }
+
+  deletePropiedad(id: number): Promise<void> {  
+    return axios.delete<void>(`${this.proyectoUrlPropiedades}/${id}`)
+      .then(response => response.data)
+      .catch((error) => {
+        console.error('Error eliminando propiedad:', error);
+        throw error;
+      });
+  }
+
+  putPropiedad(propiedad: Propiedad): Promise<Propiedad> {
+    return axios.put<Propiedad>(`${this.proyectoUrlPropiedades}/${propiedad.id}`, propiedad)
+      .then(response => response.data)
+      .catch((error) => {
+        console.error('Error actualizando propiedad con ID ${id}:', error);
+        throw error;
+      });
+  }
+
+  getPropiedadById(id: number): Promise<Propiedad> {
+    return axios.get<Propiedad>(`${this.proyectoUrlPropiedades}/${id}`)
+      .then(response => response.data)
+      .catch((error) => {
+        console.error('Error obteniendo propiedad por ID:', error);
+        throw error;
       });
   }
 }
